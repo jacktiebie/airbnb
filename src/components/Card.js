c1b2206c220e13d19d3d0ac993382b736c0d7776
Jack <jacktiebie45@gmail.com>
2022-04-17 17:52:23 +0200

    code

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import Katie from '../images/katie.png'
+import Star from '../images/star.png'
 
 export default function Card() {
     return (
-        <h1>Card</h1>
+        <div className='card-container'>
+        <img src={Katie} alt="Image of Katie Zaferes" className="card-image" />
+        <p className="sold-out">SOLD OUT</p>
+        <div className="card-flex-container">
+        <img src={Star} alt="Star" className="card-star" />
+        <p className="card-rating">5.0</p>
+        <p className="card-gray">(6) * USA</p>
+        </div>
+        <p>Life lessons with Katie Zaferes</p>
+        <p><strong>From $136</strong> / person</p>
+        </div>
     )
 }
+
